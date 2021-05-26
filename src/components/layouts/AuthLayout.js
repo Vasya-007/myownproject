e51251f5c99587b4c666a9ed707672598228d669
Vasya-007 <boyb94115@gmail.com>
2021-05-26 15:49:27 +0300

    login

--- src/components/layouts/AuthLayout.js
+++ src/components/layouts/AuthLayout.js
@@ -1,11 +1,10 @@
 import {
-  Button,
   Container,
   Nav, Navbar,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import paths from '../../router/route-paths';
-import AuthManager from '../../services/AuthManager';
+
 import React from 'react';
 
 export default function AuthLayout({ children }) {
@@ -18,11 +17,8 @@ export default function AuthLayout({ children }) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            {/* <Nav.Link as={Link} to={paths.login}>Login</Nav.Link>
-            <Nav.Link as={Link} to={paths.signup}>Signup</Nav.Link> */}
-            <Button onClick={() => AuthManager.login('token-tok')}>
-              Login
-            </Button>
+            <Nav.Link as={Link} to={paths.login}>Login</Nav.Link>
+            {/* <Nav.Link as={Link} to={paths.signup}>Signup</Nav.Link>  */}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,37 @@
 import { Link } from 'react-router-dom';
+import React from 'react';
 import {
   Card, Row, Col, Button, Form,
 } from 'react-bootstrap';
 import paths from '../router/route-paths';
-import React from 'react';
+import AuthManager from '../services/AuthManager';
+import {Component} from 'react';
 
-export default function Login() {
+export default class Login extends Component {
+  constructor(props) 
+  {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+ onChangeEmail = (e) => {
+this.setState({email: e.target.value});
+ };
+
+ onChangePassword = (e) => {
+  this.setState({password: e.target.value});
+};
+
+  onSubmit = () => {
+    const {email} = this.state;
+    console.log(`Logining in as email ${email}`)
+    AuthManager.login();
+  }
+  render () {
+    const {email, password} = this.state
+    console.log({email, password})
   return (
     <Row>
       <Col sm={{ span: 12 }} md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
@@ -17,21 +43,24 @@ export default function Login() {
               </Button>
             </Link>
             <Card.Title>Sign in</Card.Title>
+            <Form onSubmit={this.onSubmit}>
             <Form.Group>
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control value={email} onChange={this.onChangeEmail} type="email" placeholder="Enter email" />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control value={password} onChange={this.onChangePassword} type="password" placeholder="Password" />
             </Form.Group>
-            <Button variant="primary" type="submit" block>
+              <Button variant="primary" type="submit" block>
               Submit
-            </Button>
+              </Button>
+            </Form>
           </Card.Body>
         </Card>
       </Col>
     </Row>
   );
+}
 }
